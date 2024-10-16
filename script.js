4b826526d0b0f1e24bb508a63b739394d7c6d535
BossAleks123 <Aleksandartesinic720@gmail.com>
2024-10-16 21:59:17 +0200

    made the look more minimalistic and clean

--- script.js
+++ script.js
@@ -14,13 +14,17 @@ document.addEventListener("DOMContentLoaded", function() {
     let nine = document.getElementById("nine");
     let zero = document.getElementById("zero");
 
-    let savingString ="";
-    let savingNumbersLines=[];
+    //Saving the numbers for last calculation (for result[sum])
+    let savingNumbers ="";
 
+    let plusNumbers = [];
+    let minusNumbers = [];
 
-    //right-side-calculator
+    let lastOperator = "";
+    let newOperator = "";
+
+    //Right-side-calculator
     let submitButton = document.getElementById("submitButton");
-
 
     //Operators
     let plus = document.getElementById("plus");
@@ -28,177 +32,53 @@ document.addEventListener("DOMContentLoaded", function() {
     let multiply = document.getElementById("multiply");
     let divide = document.getElementById("divide");
 
-    //booleans
-    let plusOperatorHasBeenChoosen = false;
-    let minusOperatorHasBeenChoosen = false;
-    let mulitplicationOperatorHasBeenChoosen = false;
-    let divisionOperatorHasBeenChoosen = false;
-
 
     one.addEventListener("click", function(){
         savingNumbers += "1";
-        if(result === 0 && minusOperatorHasBeenChoosen === false){
-            result = 1;
-        }
-
         console.log(savingNumbers);
     })
     two.addEventListener("click", function(){
         savingNumbers += "2";
-        if(result === 0 && minusOperatorHasBeenChoosen === false){
-            result = 2;
-        }
-
-        if(result != 0 || minusOperatorHasBeenChoosen === true){
-            if(plusOperatorHasBeenChoosen === true){result += 2; plusOperatorHasBeenChoosen = false;}
-            if(minusOperatorHasBeenChoosen === true){result -= 2; minusOperatorHasBeenChoosen = false;}
-            if(mulitplicationOperatorHasBeenChoosen === true){result *= 2; mulitplicationOperatorHasBeenChoosen = false;}
-            if(divisionOperatorHasBeenChoosen === true){result /= 2; divisionOperatorHasBeenChoosen = false;}
-        }
-
-        console.log(result);
         console.log(savingNumbers);
     })
     three.addEventListener("click", function(){
         savingNumbers += "3";
-        if(result === 0 && minusOperatorHasBeenChoosen === false){
-            result = 3;
-        }
-
-        if(result != 0 || minusOperatorHasBeenChoosen === true){
-            if(plusOperatorHasBeenChoosen === true){result += 3; plusOperatorHasBeenChoosen = false;}
-            if(minusOperatorHasBeenChoosen === true){result -= 3; minusOperatorHasBeenChoosen = false;}
-            if(mulitplicationOperatorHasBeenChoosen === true){result *= 3; mulitplicationOperatorHasBeenChoosen = false;}
-            if(divisionOperatorHasBeenChoosen === true){result /= 3; divisionOperatorHasBeenChoosen = false;}
-        }
-
-        console.log(result);
         console.log(savingNumbers);
     })
     four.addEventListener("click", function(){
         savingNumbers += "4";
-        if(result === 0 && minusOperatorHasBeenChoosen === false){
-            result = 4;
-        }
-
-        if(result != 0 || minusOperatorHasBeenChoosen === true){
-            if(plusOperatorHasBeenChoosen === true){result += 4; plusOperatorHasBeenChoosen = false;}
-            if(minusOperatorHasBeenChoosen === true){result -= 4; minusOperatorHasBeenChoosen = false;}
-            if(mulitplicationOperatorHasBeenChoosen === true){result *= 4; mulitplicationOperatorHasBeenChoosen = false;}
-            if(divisionOperatorHasBeenChoosen === true){result /= 4; divisionOperatorHasBeenChoosen = false;}
-        }
-
-        console.log(result);
         console.log(savingNumbers);
     })
     five.addEventListener("click", function(){
         savingNumbers += "5";
-        if(result === 0 && minusOperatorHasBeenChoosen === false){
-            result = 5;
-        }
-
-        if(result != 0 || minusOperatorHasBeenChoosen === true){
-            if(plusOperatorHasBeenChoosen === true){result += 5; plusOperatorHasBeenChoosen = false;}
-            if(minusOperatorHasBeenChoosen === true){result -= 5; minusOperatorHasBeenChoosen = false;}
-            if(mulitplicationOperatorHasBeenChoosen === true){result *= 5; mulitplicationOperatorHasBeenChoosen = false;}
-            if(divisionOperatorHasBeenChoosen === true){result /= 5; divisionOperatorHasBeenChoosen = false;}
-        }
-
-        console.log(result);
         console.log(savingNumbers);
     })
     six.addEventListener("click", function(){
         savingNumbers += "6";
-        if(result === 0 && minusOperatorHasBeenChoosen === false){
-            result = 6;
-        }
-
-        if(result != 0 || minusOperatorHasBeenChoosen === true){
-            if(plusOperatorHasBeenChoosen === true){result += 6; plusOperatorHasBeenChoosen = false;}
-            if(minusOperatorHasBeenChoosen === true){result -= 6; minusOperatorHasBeenChoosen = false;}
-            if(mulitplicationOperatorHasBeenChoosen === true){result *= 6; mulitplicationOperatorHasBeenChoosen = false;}
-            if(divisionOperatorHasBeenChoosen === true){result /= 6; divisionOperatorHasBeenChoosen = false;}
-            if(divisionOperatorHasBeenChoosen === true){result /= 5; divisionOperatorHasBeenChoosen = false;}
-        }
-
-        console.log(result);
         console.log(savingNumbers);
     })
     seven.addEventListener("click", function(){
         savingNumbers += "7";
-        if(result === 0 && minusOperatorHasBeenChoosen === false){
-            result = 7;
-        }
-
-        if(result != 0 || minusOperatorHasBeenChoosen === true){
-            if(plusOperatorHasBeenChoosen === true){result += 7; plusOperatorHasBeenChoosen = false;}
-            if(minusOperatorHasBeenChoosen === true){result -= 7; minusOperatorHasBeenChoosen = false;}
-            if(mulitplicationOperatorHasBeenChoosen === true){result *= 7; mulitplicationOperatorHasBeenChoosen = false;}
-            if(divisionOperatorHasBeenChoosen === true){result /= 7; divisionOperatorHasBeenChoosen = false;}
-            if(divisionOperatorHasBeenChoosen === true){result /= 5; divisionOperatorHasBeenChoosen = false;}
-        }
-
-        console.log(result);
         console.log(savingNumbers);
     })
     eight.addEventListener("click", function(){
         savingNumbers += "8";
-        if(result === 0 && minusOperatorHasBeenChoosen === false){
-            result = 8;
-        }
-
-        if(result != 0 || minusOperatorHasBeenChoosen === true){
-            if(plusOperatorHasBeenChoosen === true){result += 8; plusOperatorHasBeenChoosen = false;}
-            if(minusOperatorHasBeenChoosen === true){result -= 8; minusOperatorHasBeenChoosen = false;}
-            if(mulitplicationOperatorHasBeenChoosen === true){result *= 8; mulitplicationOperatorHasBeenChoosen = false;}
-            if(divisionOperatorHasBeenChoosen === true){result /= 8; divisionOperatorHasBeenChoosen = false;}
-            if(divisionOperatorHasBeenChoosen === true){result /= 5; divisionOperatorHasBeenChoosen = false;}
-        }
-
-        console.log(result);
         console.log(savingNumbers);
     })
     nine.addEventListener("click", function(){
         savingNumbers += "9";
-        if(result === 0 && minusOperatorHasBeenChoosen === false){
-            result = 9;
-        }
-
-        if(result != 0 || minusOperatorHasBeenChoosen === true){
-            if(plusOperatorHasBeenChoosen === true){result += 9; plusOperatorHasBeenChoosen = false;}
-            if(minusOperatorHasBeenChoosen === true){result -= 9; minusOperatorHasBeenChoosen = false;}
-            if(mulitplicationOperatorHasBeenChoosen === true){result *= 9; mulitplicationOperatorHasBeenChoosen = false;}
-            if(divisionOperatorHasBeenChoosen === true){result /= 9; divisionOperatorHasBeenChoosen = false;}
-            if(divisionOperatorHasBeenChoosen === true){result /= 5; divisionOperatorHasBeenChoosen = false;}
-        }
-
-        console.log(result);
         console.log(savingNumbers);
     })
 
     zero.addEventListener("click", function(){
         savingNumbers += "0";
-        if(result === 0 && minusOperatorHasBeenChoosen === false){
-            result = 0;
-        }
-
-        if(result != 0 || minusOperatorHasBeenChoosen === true){
-            if(plusOperatorHasBeenChoosen === true){result += 0; plusOperatorHasBeenChoosen = false;}
-            if(minusOperatorHasBeenChoosen === true){result -= 0; minusOperatorHasBeenChoosen = false;}
-            if(mulitplicationOperatorHasBeenChoosen === true){result *= 0; mulitplicationOperatorHasBeenChoosen = false;}
-            if(divisionOperatorHasBeenChoosen === true){result /= 0; divisionOperatorHasBeenChoosen = false;}
-            if(divisionOperatorHasBeenChoosen === true){result /= 5; divisionOperatorHasBeenChoosen = false;}
-        }
-
-        console.log(result);
         console.log(savingNumbers);
-
     })
 
     submitButton.addEventListener("click", function(){
-        if(result != 0 || minusOperatorHasBeenChoosen === true){
+        
             //for loop (all additions + all minus) + (all multiplications + all divisions)
-        }
+
 
         console.log(result);
     })
@@ -207,42 +87,17 @@ document.addEventListener("DOMContentLoaded", function() {
 
     //operators
     plus.addEventListener("click", function(){
-        plusOperatorHasBeenChoosen = true;
-        
-        minusOperatorHasBeenChoosen = false;
-        mulitplicationOperatorHasBeenChoosen = false;
-        divisionOperatorHasBeenChoosen = false;
-
         console.log("plus operator has been chosen");
+        if(lastOperator !== "*" && lastOperator !== "/" && newOperator !== "/" && newOperator !== "*" && lastOperator !== "-"){
+            plusNumbers[plusNumbers.length] = savingNumbers;
+            savingNumbers = "";
+        }
     })
     minus.addEventListener("click", function(){
-        minusOperatorHasBeenChoosen = true;
-
-        mulitplicationOperatorHasBeenChoosen = false;
-        divisionOperatorHasBeenChoosen = false;
-        plusOperatorHasBeenChoosen = false;
-
         console.log("plus operator has been chosen");
-    })
-    multiply.addEventListener("click", function(){
-        mulitplicationOperatorHasBeenChoosen = true;
-
-        plusOperatorHasBeenChoosen = false;
-        minusOperatorHasBeenChoosen = false;
-        divisionOperatorHasBeenChoosen = false;
-
-        console.log("plus operator has been chosen");
-    })
-    divide.addEventListener("click", function(){
-        divisionOperatorHasBeenChoosen = true;
-
-        plusOperatorHasBeenChoosen = false;
-        minusOperatorHasBeenChoosen = false;
-        mulitplicationOperatorHasBeenChoosen = false;
-
-        console.log("plus operator has been chosen");
-    })
-
-
-    
+        if(lastOperator !== "*" && lastOperator !== "/" && newOperator !== "/" && newOperator !== "*" && lastOperator !== "+"){
+            minusNumbers[minusNumbers.length] = savingNumbers;
+            savingNumbers = "";
+        }
+    })    
 });
